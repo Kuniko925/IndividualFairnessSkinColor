24ac1959618718ca500b9b367d77230c8ca9bb93
Kuniko Paxton <57262209+Kuniko925@users.noreply.github.com>
2024-10-27 22:14:18 +0000

    Add files via upload

---
+++ code/distance.py
@@ -0,0 +1,182 @@
+import numpy as np
+import statistics
+import scipy.stats as stats
+from scipy.stats import wasserstein_distance, ks_2samp, energy_distance
+import pennylane as qml
+
+def probability_density_function(XX, YY):
+    nx = len(XX)
+    ny = len(YY)
+    n = nx + ny
+
+    min_val = min(np.min(XX), np.min(YY))
+    max_val = max(np.max(XX), np.max(YY))
+    bins = np.linspace(min_val, max_val, n + 1)
+
+    X_pdf, _ = np.histogram(XX, bins=bins, density=True)
+    Y_pdf, _ = np.histogram(YY, bins=bins, density=True)
+
+    return X_pdf, Y_pdf
+
+class DistanceMeasure:
+    def __init__(self, XX, YY):
+        self.XX = XX
+        self.YY = YY
+
+    def check_sign(self):
+        gap = statistics.median(self.XX) - statistics.median(self.YY)
+        
+        if gap < 0:
+            return 1
+        else:
+            return -1
+
+    # Want to know positive/negative distibution shift
+    def sign_wasserstein_distance(self):
+        wd = wasserstein_distance(self.XX, self.YY)
+        sign = self.check_sign()
+        return wd * sign
+
+    def wasserstein(self):
+        wd = wasserstein_distance(self.XX, self.YY)
+        return wd
+    
+    def kuiper_distance(self):
+
+        nx = len(self.XX)
+        ny = len(np.array(self.YY))
+        n = nx + ny
+    
+        XY = np.concatenate([self.XX, self.YY])
+        X2 = np.concatenate([np.repeat(1/nx, nx), np.repeat(0, ny)])
+        Y2 = np.concatenate([np.repeat(0, nx), np.repeat(1/ny, ny)])
+    
+        S_Ind = np.argsort(XY)
+        XY_Sorted = XY[S_Ind]
+        X2_Sorted = X2[S_Ind]
+        Y2_Sorted = Y2[S_Ind]
+    
+        up = 0
+        down = 0
+        Res = 0
+        E_CDF = 0
+        F_CDF = 0
+        height = 0
+        power = 1
+    
+        for ii in range(0, n-2):
+            E_CDF = E_CDF + X2_Sorted[ii]
+            F_CDF = F_CDF + Y2_Sorted[ii]
+            if XY_Sorted[ii+1] != XY_Sorted[ii]: height = F_CDF-E_CDF
+            if height > up: up = height
+            if height < down: down = height
+    
+        K_Dist = abs(down)**power + abs(up)**power
+    
+        return K_Dist
+
+    def anderson_darling_distance(self):
+    
+        nx = len(self.XX)
+        ny = len(np.array(self.YY))
+        n = nx + ny
+    
+        XY = np.concatenate([self.XX, self.YY])
+        X2 = np.concatenate([np.repeat(1/nx, nx), np.repeat(0, ny)])
+        Y2 = np.concatenate([np.repeat(0, nx), np.repeat(1/ny, ny)])
+    
+        S_Ind = np.argsort(XY)
+        XY_Sorted = XY[S_Ind]
+        X2_Sorted = X2[S_Ind]
+        Y2_Sorted = Y2[S_Ind]
+    
+        Res = 0
+        E_CDF = 0
+        F_CDF = 0
+        G_CDF = 0
+        height = 0
+        SD = 0
+        power = 1
+    
+        for ii in range(0, n-2):
+            E_CDF = E_CDF + X2_Sorted[ii]
+            F_CDF = F_CDF + Y2_Sorted[ii]
+            G_CDF = G_CDF + 1/n
+            SD = (n * G_CDF * (1-G_CDF))**0.5
+            height = abs(F_CDF - E_CDF)
+            if XY_Sorted[ii+1] != XY_Sorted[ii]:
+                if SD>0:
+                    Res = Res + (height/SD)**power
+    
+        AD_Dist = Res
+    
+        return AD_Dist
+
+    def cvm_distance(self):
+    
+        nx = len(self.XX)
+        ny = len(self.YY)
+        n = nx + ny
+    
+        XY = np.concatenate([self.XX, self.YY])
+        X2 = np.concatenate([np.repeat(1/nx, nx), np.repeat(0, ny)])
+        Y2 = np.concatenate([np.repeat(0, nx), np.repeat(1/ny, ny)])
+    
+        S_Ind = np.argsort(XY)
+        XY_Sorted = XY[S_Ind]
+        X2_Sorted = X2[S_Ind]
+        Y2_Sorted = Y2[S_Ind]
+    
+        Res = 0;
+        E_CDF = 0;
+        F_CDF = 0;
+        power = 1;
+    
+        for ii in range(0, n-2):
+            E_CDF = E_CDF + X2_Sorted[ii]
+            F_CDF = F_CDF + Y2_Sorted[ii]
+            height = abs(F_CDF - E_CDF)
+            if XY_Sorted[ii+1] != XY_Sorted[ii]: Res = Res + height**power
+    
+        CVM_Dist = Res
+    
+        return CVM_Dist
+
+    def kolmogorov_smirnov(self):
+        res = ks_2samp(self.XX, self.YY)
+        return res.statistic
+
+    def energy_distance(self):
+        return energy_distance(self.XX, self.YY)
+
+    
+
+    def kruglov_distance(self):
+        X_pdf, Y_pdf = probability_density_function(self.XX, self.YY)
+        X_cdf = np.cumsum(X_pdf)
+        Y_cdf = np.cumsum(Y_pdf)
+        distance = np.max(np.abs(X_cdf - Y_cdf))
+        return distance
+
+    # The fidelity similarity (or Bhattacharya coefficient, Hellinger affinity)
+    def fidelity_similarity(self):
+        X_pdf, Y_pdf = probability_density_function(self.XX, self.YY)
+        result = np.sum(np.sqrt(X_pdf * Y_pdf))
+        return result
+
+    def harmonic_mean_similarity(self):
+        X_pdf, Y_pdf = probability_density_function(self.XX, self.YY)
+         # Adding a tiny number to avoid a zero division
+        epsilon = 1e-10
+        result = 2 * np.sum((X_pdf * Y_pdf) / (X_pdf + Y_pdf + epsilon))
+        return result
+
+    def hellinger_metric(self):
+        X_pdf, Y_pdf = probability_density_function(self.XX, self.YY)
+        result = np.sum((np.sqrt(X_pdf) - np.sqrt(Y_pdf))**2)/2
+        return result
+
+    def patrick_fisher_distance(self):
+        X_pdf, Y_pdf = probability_density_function(self.XX, self.YY)
+        result = np.sum(np.abs(X_pdf - Y_pdf)**2)/2
+        return result
